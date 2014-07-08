d91c127a7ba134550a5b65cf529e9a52e57c47bd
tiendan <tiendan@gmail.com>
2014-07-08 12:50:52 +0200

    Fix static file settings

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -151,14 +151,14 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..', 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, '..', 'static'),
+#)
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
