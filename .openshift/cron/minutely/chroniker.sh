7024484647bff3c8e942574f3de57d884e97f848
tiendan <tiendan@gmail.com>
2014-09-14 04:53:35 +0200

    Updating server

--- .openshift/cron/minutely/chroniker.sh
+++ .openshift/cron/minutely/chroniker.sh
@@ -1,5 +1,1 @@
-* * * * * echo $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/chroniker
-* * * * * ls -l $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/chroniker
-* * * * * echo $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/activate_this.py
-* * * * * ls -l $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/activate_this.py 
-* * * * * $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/chroniker -e $OPENSHIFT_HOMEDIR/app-root/runtime/dependencies/python/virtenv/bin/activate_this.py 
+* * * * * chroniker -e activate_this.py 
